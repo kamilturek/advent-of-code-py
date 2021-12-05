bd592c5c254035a8bf5f3cfb2737a003b13a4141
Kamil Turek <kamil.turek@hotmail.com>
2021-12-05 15:20:09 +0100

    Add solutions for day two

---
+++ puzzles/02_part1.py
@@ -0,0 +1,28 @@
+from typing import Literal
+
+
+Direction = Literal['forward', 'up', 'down']
+
+
+def parse(input_data: str) -> list[tuple[Direction, int]]:
+    return list(
+        map(
+            lambda command: (command[0], int(command[1])),
+            [line.split() for line in input_data.splitlines()],
+        )
+    )
+
+
+def solve(input_data: str) -> int:
+    commands = parse(input_data)
+
+    route = {
+        'forward': 0,
+        'down': 0,
+        'up': 0,
+    }
+
+    for direction, units in commands:
+        route[direction] += units
+
+    return route['forward'] * (route['down'] - route['up'])
---
+++ puzzles/02_part2.py
@@ -0,0 +1,32 @@
+from typing import Literal
+
+
+Direction = Literal['forward', 'up', 'down']
+
+def parse(input_data: str) -> list[tuple[Direction, int]]:
+    return list(
+        map(
+            lambda command: (command[0], int(command[1])),
+            [line.split() for line in input_data.splitlines()],
+        )
+    )
+
+
+def solve(input_data: str) -> int:
+    commands = parse(input_data)
+
+    horizontal_pos = 0
+    depth = 0
+    aim = 0
+
+    for direction, units in commands:
+        match direction:
+            case 'forward':
+                horizontal_pos += units
+                depth += aim * units
+            case 'up':
+                aim -= units
+            case 'down':
+                aim += units
+
+    return horizontal_pos * depth
